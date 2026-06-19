b7985a5872f726565e98ffabb19f6be93ba8d2af
Davimp0413 <davitheorigamiking@gmail.com>
2026-06-19 17:35:35 -0300

    Add files via upload

---
+++ login sexta 16h/app.js
@@ -0,0 +1,8 @@
+const senho = document.querySelector(".senha");
+const forco = document.querySelector(".forca");
+const texta_forco = document.querySelector(".texto-forca");
+const botao = document.querySelector(".teste");
+
+botao.addEventListener("click",() => {
+    texta_forco.innerHTML = "ALAKAZAM"
+})
